42e7521249a174dac6abea3609e0788bac00f48f
Rafael E. Cortez <rafael.ecortez@gmail.com>
2023-07-12 03:15:03 -0300

    msvc_books added

---
+++ msvc_books/src/main/java/com/msvc/books/MsvcBooksApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcBooksApplication.class, args);
+    }
+
+}
---
+++ msvc_books/src/test/java/com/msvc/books/MsvcBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
